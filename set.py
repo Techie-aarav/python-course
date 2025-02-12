89106895c1c3f42db68807b6d6bd106d9e99dcf8
Techie-aarav <madanaarav20@gmail.com>
2025-02-12 15:23:44 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,38 @@
+#Activity 2
+colour1Set = {"green" , "blue"}
+colour2Set = {"yellow" , "blue"}
+print("Orignal set elements:")
+print(colour1Set)
+print(colour2Set)
+print("\n Intersection of the two said sets:")
+commonSet = colour1Set.intersection(colour2Set)
+print(commonSet)
+
+#Activity 3
+
+print("\n Union of the two said sets:")
+unionSet = colour1Set.union(colour2Set)
+print(unionSet)
+
+
+
+
+
+
+#Activity 1
+intSet = {10,20,30,40}
+print(intSet)
+datatypeSet = {"aarav" , 43.9 , "copdingal" , True , False}
+print(datatypeSet)
+my_set = {1,2,3,4,3,2,4}
+print(my_set)
+xSet = {1,2,3,2}
+print(xSet)
+ySet = {0,1,3,4,5}
+print(ySet)
+print("Orignal set:")
+print(ySet)
+ySet.pop()
+print("After removing the first element from the first set.")
+print(ySet,"\n" )
+
